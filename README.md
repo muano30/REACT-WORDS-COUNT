dec298a36273d8a93a8bf5b091734ae664e6b888
muano30 <muanonevhufumba@gmail.com>
2022-01-25 13:46:47 +0200

    words count

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # REACT-WORDS-COUNT
 
 
-A user must be able to submit the text they have typed on the textarea, and also know how many words they have submitted.
+A user must be able to submitt the text they have typed on the textarea, and also know how many words they have submitted. The form need to display the text and the number of words typed by the user... Textarea must take 150 or less words not character.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+function WordsCounter() {
+  const [words, setWords] = useState('');
+  const [wordsInfo, setWordsInfo] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let wordLength = words.trim().split(' ').length
+    // console.log('word', words.trim().split(' '))
+    if (!words) {
+      return alert("Type something")
+    }
+
+    if (wordLength > 150) {
+      return alert("Maximum Words 150 Exceeded")
+    }
+
+    else {
+setWordsInfo([...wordsInfo, WordCount(words)])
+    }
+    setWords("")
+    // console.log('counter',WordCount(words))
+  }
+
+  function WordCount(str) {
+    return {
+      word: str, wordCount: str.split(' ')
+        .filter(function (n) { return n != '' })
+        .length
+    };
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WORDS COUNTER</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <textarea
+            value={words}
+            placeholder='Discribe yourself here.....'
+            onChange={(e) => setWords(e.target.value)}
+            disabled={words.split(' ').length > 150}
+          ></textarea>
+        </div>
+
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+
+
+      </form>
+
+      {wordsInfo.map((item, index) => {
+        return (
+          <ul>
+            <li key={index}>BLOG: {item.word}</li>
+            <li>WORDS COUNT: {item.wordCount}</li>
+          </ul>
+        )
+
+      })}
     </div>
   );
 }
 
-export default App;
+export default WordsCounter;
